82a52e6f98b35cac9d8777e8c3addb266ffda61e
AlegDreg <80702802+AlegDreg@users.noreply.github.com>
2022-02-22 14:06:07 +0300

    Add files via upload

---
+++ Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Users
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int Password { get; set; }
+        public int Time { get; set; }
+    }
+}
